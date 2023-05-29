14aa4e39c4bdc378d97e10ba2ed7a63f95f471fe
niblit <github.com/niblit>
2023-05-29 12:25:35 -0600

    Moved pieces mod to the proper location

--- assets/src/lib.rs
+++ assets/src/lib.rs
@@ -1,17 +1,1 @@
-pub mod pieces {
-    pub mod california {
-        pub const WHITE_KING: &[u8] = include_bytes!("pieces/california/WhiteKing.png");
-        pub const WHITE_QUEEN: &[u8] = include_bytes!("pieces/california/WhiteQueen.png");
-        pub const WHITE_ROOK: &[u8] = include_bytes!("pieces/california/WhiteRook.png");
-        pub const WHITE_BISHOP: &[u8] = include_bytes!("pieces/california/WhiteBishop.png");
-        pub const WHITE_KNIGHT: &[u8] = include_bytes!("pieces/california/WhiteKnight.png");
-        pub const WHITE_PAWN: &[u8] = include_bytes!("pieces/california/WhitePawn.png");
-
-        pub const BLACK_KING: &[u8] = include_bytes!("pieces/california/BlackKing.png");
-        pub const BLACK_QUEEN: &[u8] = include_bytes!("pieces/california/BlackQueen.png");
-        pub const BLACK_ROOK: &[u8] = include_bytes!("pieces/california/BlackRook.png");
-        pub const BLACK_BISHOP: &[u8] = include_bytes!("pieces/california/BlackBishop.png");
-        pub const BLACK_KNIGHT: &[u8] = include_bytes!("pieces/california/BlackKnight.png");
-        pub const BLACK_PAWN: &[u8] = include_bytes!("pieces/california/BlackPawn.png");
-    }
-}
+pub mod pieces;
---
+++ assets/src/pieces/mod.rs
@@ -0,0 +1,15 @@
+pub mod california {
+    pub const WHITE_KING: &[u8] = include_bytes!("california/WhiteKing.png");
+    pub const WHITE_QUEEN: &[u8] = include_bytes!("california/WhiteQueen.png");
+    pub const WHITE_ROOK: &[u8] = include_bytes!("california/WhiteRook.png");
+    pub const WHITE_BISHOP: &[u8] = include_bytes!("california/WhiteBishop.png");
+    pub const WHITE_KNIGHT: &[u8] = include_bytes!("california/WhiteKnight.png");
+    pub const WHITE_PAWN: &[u8] = include_bytes!("california/WhitePawn.png");
+
+    pub const BLACK_KING: &[u8] = include_bytes!("california/BlackKing.png");
+    pub const BLACK_QUEEN: &[u8] = include_bytes!("california/BlackQueen.png");
+    pub const BLACK_ROOK: &[u8] = include_bytes!("california/BlackRook.png");
+    pub const BLACK_BISHOP: &[u8] = include_bytes!("california/BlackBishop.png");
+    pub const BLACK_KNIGHT: &[u8] = include_bytes!("california/BlackKnight.png");
+    pub const BLACK_PAWN: &[u8] = include_bytes!("california/BlackPawn.png");
+}
