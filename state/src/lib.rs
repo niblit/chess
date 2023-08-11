755ad196b386c6c92aa0bfb2b37de26d3973bb18
niblit <niblit.gh@gmail.com>
2023-08-11 16:30:41 -0600

    Renamed module

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -1,11 +1,11 @@
 mod board;
-mod game;
+mod components;
 mod game_state;
 mod moves;
 
 pub mod prelude {
     pub use crate::board::{BoardCoordinates, INITIAL_POSITION};
-    pub use crate::game::{CastlingRights, GameResult, Piece, Player, Square};
+    pub use crate::components::{CastlingRights, GameResult, Piece, Player, Square};
     pub use crate::game_state::GameState;
     pub use crate::moves::{Move, MoveCounter, SpecialMove};
 }
