1a0b3d7fbcf2ae6bd06a0b34e7c21b79fba0ad4c
niblit <niblit.gh@gmail.com>
2023-08-11 16:27:20 -0600

    Renamed pieces assets

--- assets/src/pieces/california/mod.rs
+++ assets/src/pieces/california/mod.rs
@@ -1,14 +1,14 @@
 /// Piece set from lichess
-pub const WHITE_KING: &[u8] = include_bytes!("WhiteKing.png");
-pub const WHITE_QUEEN: &[u8] = include_bytes!("WhiteQueen.png");
-pub const WHITE_ROOK: &[u8] = include_bytes!("WhiteRook.png");
-pub const WHITE_BISHOP: &[u8] = include_bytes!("WhiteBishop.png");
-pub const WHITE_KNIGHT: &[u8] = include_bytes!("WhiteKnight.png");
-pub const WHITE_PAWN: &[u8] = include_bytes!("WhitePawn.png");
+pub const WHITE_KING: &[u8] = include_bytes!("wK.png");
+pub const WHITE_QUEEN: &[u8] = include_bytes!("wQ.png");
+pub const WHITE_ROOK: &[u8] = include_bytes!("wR.png");
+pub const WHITE_BISHOP: &[u8] = include_bytes!("wB.png");
+pub const WHITE_KNIGHT: &[u8] = include_bytes!("wN.png");
+pub const WHITE_PAWN: &[u8] = include_bytes!("wP.png");
 
-pub const BLACK_KING: &[u8] = include_bytes!("BlackKing.png");
-pub const BLACK_QUEEN: &[u8] = include_bytes!("BlackQueen.png");
-pub const BLACK_ROOK: &[u8] = include_bytes!("BlackRook.png");
-pub const BLACK_BISHOP: &[u8] = include_bytes!("BlackBishop.png");
-pub const BLACK_KNIGHT: &[u8] = include_bytes!("BlackKnight.png");
-pub const BLACK_PAWN: &[u8] = include_bytes!("BlackPawn.png");
+pub const BLACK_KING: &[u8] = include_bytes!("bK.png");
+pub const BLACK_QUEEN: &[u8] = include_bytes!("bQ.png");
+pub const BLACK_ROOK: &[u8] = include_bytes!("bR.png");
+pub const BLACK_BISHOP: &[u8] = include_bytes!("bB.png");
+pub const BLACK_KNIGHT: &[u8] = include_bytes!("bN.png");
+pub const BLACK_PAWN: &[u8] = include_bytes!("bP.png");
